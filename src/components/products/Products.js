0a617e8052455cade8a8730e23950322419511bd
samane zarini <samane_zarini@yahoo.com>
2024-12-13 21:33:50 +0330

    product hover done

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -9,29 +9,42 @@ import pro7 from '../../assets/images/products/7.jpg';
 import pro8 from '../../assets/images/products/8.jpg';
 import pro9 from '../../assets/images/products/9.jpg';
 import pro10 from '../../assets/images/products/10.jpg';
+import { useState } from "react";
 
 function Products() {
 
+    const productImages = [pro1, pro2, pro3, pro4, pro5, pro6, pro7, pro8, pro9, pro10];
+
     const products = [
-        { id: 1, image: pro1, discount: '-10%', tags: ['new'], priceOff:"17.00",  price: "15.30" },
-        { id: 2, image: pro2, discount: '', tags: [], priceOff:"",  price: "15.00" },
-        { id: 3, image: pro10, discount: '-20%', tags: [], priceOff:"30.00",  price: "24.00" },
-        { id: 4, image: pro4, discount: '-12%', tags: ['new'], priceOff:"20.00",  price: "17.60" },
-        { id: 5, image: pro5, discount: '-5%', tags: [], priceOff:"12.00",  price: "11.40" },
-        { id: 6, image: pro6, discount: '-10%', tags: ['out'], priceOff:"12.00",  price: "10.80" },
-        { id: 7, image: pro7, discount: '', tags: ['new'], priceOff:"",  price: "15.00" },
-        { id: 8, image: pro8, discount: '-12%', tags: ['new'], priceOff:"17.00",  price: "14.96" },
-        { id: 9, image: pro9, discount: '', tags: ['new'], priceOff:"",  price: "16.00" },
+        { id: 1, image: pro1, discount: '-10%', tags: ['new'], priceOff: "17.00", price: "15.30" },
+        { id: 2, image: pro2, discount: '', tags: [], priceOff: "", price: "15.00" },
+        { id: 3, image: pro10, discount: '-20%', tags: [], priceOff: "30.00", price: "24.00" },
+        { id: 4, image: pro4, discount: '-12%', tags: ['new'], priceOff: "20.00", price: "17.60" },
+        { id: 5, image: pro5, discount: '-5%', tags: [], priceOff: "12.00", price: "11.40" },
+        { id: 6, image: pro6, discount: '-10%', tags: ['out'], priceOff: "12.00", price: "10.80" },
+        { id: 7, image: pro7, discount: '', tags: ['new'], priceOff: "", price: "15.00" },
+        { id: 8, image: pro8, discount: '-12%', tags: ['new'], priceOff: "17.00", price: "14.96" },
+        { id: 9, image: pro9, discount: '', tags: ['new'], priceOff: "", price: "16.00" },
     ];
+
+    const [hoveredId, setHoveredId] = useState(null);
 
     return (
         <>
             <NavItem />
             <div className="container mx-auto">
                 <div className="grid grid-cols-3 gap-6">
-                    {products.map((product) => (
-                        <div key={product.id} className="relative bg-white overflow-hidden mb-4">
-                            <img src={product.image} alt={`Product ${product.id}`} className="object-cover" />
+                    {products.map((product, index) => (
+                        <div
+                            key={product.id}
+                            className="relative bg-white overflow-hidden mb-4"
+                            onMouseEnter={() => setHoveredId(index)}
+                            onMouseLeave={() => setHoveredId(null)}
+                        >
+                            <img
+                                src={hoveredId === index ? productImages[productImages.length - 1 - index] : productImages[index]}
+                                alt={`Product ${product.id}`}
+                                className="object-cover transition-all duration-500 ease-in-out" />
                             <div className="absolute top-6 left-6 flex flex-col text-center space-y-2">
                                 {product.discount && <span className="bg-[#98d9cb] text-white text-xs font-semibold px-3 py-4 rounded-full">{product.discount}</span>}
                                 {product.tags.includes('new') && <span className="bg-[#c71a34] text-white text-xs font-semibold px-3 py-4 rounded-full">new</span>}
