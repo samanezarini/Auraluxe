a405b46a2f89764519d8633f0e42cffe4e9b2b44
samane zarini <samane_zarini@yahoo.com>
2024-12-15 21:19:45 +0330

    navitem done

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -40,7 +40,7 @@ function Products() {
 
     return (
         <>
-            <NavItem setFilter={setFilter} />
+            <NavItem setFilter={setFilter} filter={filter} />
             <div className="container mx-auto mb-20">
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 md:grid-cols-2 gap-6">
                     {filteredProducts.map((product, index) => (
--- src/components/products/navitem/NavItem.js
+++ src/components/products/navitem/NavItem.js
@@ -1,21 +1,34 @@
-
-function NavItem({ setFilter }) {
-
+function NavItem({ setFilter, filter }) {
     return (
         <div className="container mx-auto border-b-2 m-5 border-gray-200 dark:border-neutral-700">
             <nav className="-mb-0.5 flex justify-center gap-x-8">
-                <button onClick={() => setFilter("new")} className="py-4 px-1 border-b-2 border-transparent text-2xl text-gray-500 hover:text-gray-800">
+                <button
+                    onClick={() => setFilter("new")}
+                    className={`py-4 px-1 border-b-2 ${
+                        filter === "new" ? "border-red-500 text-gray-800" : "border-transparent text-gray-500"
+                    } text-2xl hover:text-gray-800`}
+                >
                     New
                 </button>
-                <button onClick={() => setFilter("all")} className="py-4 px-1 border-b-2 border-transparent text-2xl text-gray-500 hover:text-gray-800">
+                <button
+                    onClick={() => setFilter("all")}
+                    className={`py-4 px-1 border-b-2 ${
+                        filter === "all" ? "border-red-500 text-gray-800" : "border-transparent text-gray-500"
+                    } text-2xl hover:text-gray-800`}
+                >
                     Popular
                 </button>
-                <button onClick={() => setFilter("sale")} className="py-4 px-1 border-b-2 border-transparent text-2xl text-gray-500 hover:text-gray-800">
+                <button
+                    onClick={() => setFilter("sale")}
+                    className={`py-4 px-1 border-b-2 ${
+                        filter === "sale" ? "border-red-500 text-gray-800" : "border-transparent text-gray-500"
+                    } text-2xl hover:text-gray-800`}
+                >
                     Sale
                 </button>
             </nav>
         </div>
-    )
+    );
 }
 
 export default NavItem;
